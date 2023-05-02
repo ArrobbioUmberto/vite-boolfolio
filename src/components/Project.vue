46599ddc3108cc5432108430e4095b72fc21c502
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-02 17:30:25 +0200

    FEAT create project and set methods+axios

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,8 +1,35 @@
 <script>
+import axios from 'axios'
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+    data() {
+        return {
+            projects: []
+        };
+    },
+    methods: {
+        fetchProjects() {
+            axios.get("http://127.0.0.1:8000/api/projects")
+                .then(res => {
+                    // console.log(res)
+                    const results = res.data.results;
+                    this.projects = results.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    },
+    mounted() {
+        this.fetchProjects();
+    },
+    components: { ProjectCard }
+}
 </script>
 <template>
-    <div class="container">
-        <h2>ciao ma piccolo</h2>
+    <div class="container d-flex">
+        <ProjectCard v-for="project in projects" :key="project.id" :project="project"></ProjectCard>
     </div>
 </template>
-<style></style>
+<style scoped></style>
