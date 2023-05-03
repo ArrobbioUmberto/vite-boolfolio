274e14f4f0d0c391dd487081bbb1dfa26c7f2741
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-03 16:41:59 +0200

    FEAT create new pages portfolio

---
+++ src/pages/Portfolio.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div>
+        <div class="container">
+            <h1>PORTFOLIO</h1>
+        </div>
+    </div>
+</template>
+<style></style>
