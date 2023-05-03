2267aa290a4299b63c9c696126cb024cb8a4fcdd
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-03 16:41:26 +0200

    FEAT create navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <header>
+        <div>
+            <div class="container nav">
+                <router-link :to="{ name: 'home' }">MyProjects</router-link>
+                <ul class="nav__menu">
+                    <li>
+                        <router-link :to="{ name: 'home' }">Home</router-link>
+                    </li>
+                    <li><router-link :to="{ name: 'portfolio' }">Projects</router-link></li>
+                    <li><router-link :to="{ name: 'about' }">AboutUs</router-link></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+<style>
+.nav,
+.nav__menu {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+}
+
+.nav__menu {
+    margin-left: auto;
+}
+</style>
