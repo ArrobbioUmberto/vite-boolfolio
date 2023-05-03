e92c5f53c1e402c342f14cae5ee537e306b6607c
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-03 16:18:51 +0200

    FEAT create home vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div>
+        <div class="container">
+            <h1>HOME</h1>
+        </div>
+    </div>
+</template>
+<style></style>
