539c9549c3baf91fd78566f427b5287f24b604eb
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-03 17:06:06 +0200

    FEAT change text about us and boolfolio

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,13 +7,13 @@ export default {
     <header>
         <div>
             <div class="container nav">
-                <router-link :to="{ name: 'home' }">MyProjects</router-link>
+                <router-link :to="{ name: 'home' }">Boolfolio</router-link>
                 <ul class="nav__menu">
                     <li>
                         <router-link :to="{ name: 'home' }">Home</router-link>
                     </li>
                     <li><router-link :to="{ name: 'portfolio' }">Projects</router-link></li>
-                    <li><router-link :to="{ name: 'about' }">AboutUs</router-link></li>
+                    <li><router-link :to="{ name: 'about' }">About-Us</router-link></li>
                 </ul>
             </div>
         </div>
