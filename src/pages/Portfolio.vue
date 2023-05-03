7e82e597e01134525ec1642f58bdc50ef4e4c4ec
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-03 17:07:13 +0200

    FEAT import projects and add to template

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,12 +1,16 @@
 <script>
+import Project from '../components/Project.vue';
 export default {
-
+    components: {
+        Project
+    }
 }
 </script>
 <template>
     <div>
         <div class="container">
             <h1>PORTFOLIO</h1>
+            <Project></Project>
         </div>
     </div>
 </template>
