335662e4cce1614f058f483f82828ddfd093e558
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-04 16:18:55 +0200

    FEAT create project show details

---
+++ src/pages/ProjectShow.vue
@@ -0,0 +1,20 @@
+<script>
+import Default from '../layouts/Default.vue';
+
+export default {
+    components: {
+
+        Default
+    }
+}
+</script>
+<template>
+    <Default>
+
+        <div class="container">
+            <h1>progetto in dettaglio</h1>
+        </div>
+
+    </Default>
+</template>
+<style></style>
