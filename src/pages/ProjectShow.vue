b2b0746bc490a356724c672106ff7c3b560ecb51
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-04 17:06:12 +0200

    FEAT create method for axios call

--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -1,10 +1,26 @@
 <script>
 import Default from '../layouts/Default.vue';
-
+import axios from 'axios'
 export default {
     components: {
-
         Default
+    },
+
+    methods: {
+        fetchProjects() {
+            axios.get("http://127.0.0.1:8000/api/projects/${this.$route.params.slug}").then(res => {
+                console.log(res)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    },
+    created() {
+        this.fetchProjects()
+    },
+    mounted() {
+
+        console.log('slug:', this.$route.params.slug)
     }
 }
 </script>
@@ -12,7 +28,8 @@ export default {
     <Default>
 
         <div class="container">
-            <h1>progetto in dettaglio</h1>
+            <h1>Titolo: </h1>
+            <h2>slug: {{ $route.params.slug }}</h2>
         </div>
 
     </Default>
