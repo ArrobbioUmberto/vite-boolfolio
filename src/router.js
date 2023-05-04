eb540eb4435f0c996c6d7c571071cf55cf41c523
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-04 16:18:24 +0200

    FEAT create new router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from './pages/Home.vue'
 import Portfolio from './pages/Portfolio.vue'
 import AboutUs from './pages/AboutUs.vue'
+import ProjectShow from './pages/ProjectShow.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             name: 'about',
             component: AboutUs,
         },
+        {
+            path: '/portfolio/:slug',
+            name: 'projects.show',
+            component: ProjectShow
+        }
     ],
 })
 
